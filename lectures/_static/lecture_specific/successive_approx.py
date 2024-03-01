4beb13a338007b24c20beb6bf7ba235d204ea560
kp992 <145801876+kp992@users.noreply.github.com>
2024-03-02 01:41:03 +0530

    Use JAX in successive_approx (#134)

--- lectures/_static/lecture_specific/successive_approx.py
+++ lectures/_static/lecture_specific/successive_approx.py
@@ -1,21 +1,21 @@
-def successive_approx(T,                     # Operator (callable)
-                      x_0,                   # Initial condition
-                      tolerance=1e-6,        # Error tolerance
-                      max_iter=10_000,       # Max iteration bound
-                      print_step=25,         # Print at multiples
-                      verbose=False):
-    x = x_0
-    error = tolerance + 1
-    k = 1
-    while error > tolerance and k <= max_iter:
-        x_new = T(x)
+def successive_approx_jax(x_0,                   # Initial condition
+                          constants,
+                          sizes,
+                          arrays,                 
+                          tolerance=1e-6,        # Error tolerance
+                          max_iter=10_000):      # Max iteration bound
+
+    def body_fun(k_x_err):
+        k, x, error = k_x_err
+        x_new = T(x, constants, sizes, arrays)
         error = jnp.max(jnp.abs(x_new - x))
-        if verbose and k % print_step == 0:
-            print(f"Completed iteration {k} with error {error}.")
-        x = x_new
-        k += 1
-    if error > tolerance:
-        print(f"Warning: Iteration hit upper bound {max_iter}.")
-    elif verbose:
-        print(f"Terminated successfully in {k} iterations.")
+        return k + 1, x_new, error
+
+    def cond_fun(k_x_err):
+        k, x, error = k_x_err
+        return jnp.logical_and(error > tolerance, k < max_iter)
+
+    k, x, error = jax.lax.while_loop(cond_fun, body_fun, (1, x_0, tolerance + 1))
     return x
+
+successive_approx_jax = jax.jit(successive_approx_jax, static_argnums=(2,))
--- lectures/_static/lecture_specific/vfi.py
+++ lectures/_static/lecture_specific/vfi.py
@@ -2,8 +2,7 @@
 
 def value_iteration(model, tol=1e-5):
     constants, sizes, arrays = model
-    _T = lambda v: T(v, constants, sizes, arrays)
     vz = jnp.zeros(sizes)
 
-    v_star = successive_approx(_T, vz, tolerance=tol)
+    v_star = successive_approx_jax(vz, constants, sizes, arrays, tolerance=tol)
     return get_greedy(v_star, constants, sizes, arrays)
