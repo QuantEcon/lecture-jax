463ce9f297e87988d53adfbf0215ec8d445bd352
kp992 <kpl992@outlook.com>
2024-03-06 10:55:42 +0530

    Use callback in successive_approx

--- lectures/_static/lecture_specific/vfi.py
+++ lectures/_static/lecture_specific/vfi.py
@@ -3,6 +3,6 @@
 def value_iteration(model, tol=1e-5):
     constants, sizes, arrays = model
     vz = jnp.zeros(sizes)
-
-    v_star = successive_approx_jax(vz, constants, sizes, arrays, tolerance=tol)
+    _T = lambda v: T(v, constants, sizes, arrays)
+    v_star = successive_approx_jax(_T, vz, tolerance=tol)
     return get_greedy(v_star, constants, sizes, arrays)
