a3bc43bc47578eab715051edb66d054326c79ad2
kp992 <145801876+kp992@users.noreply.github.com>
2024-03-08 00:05:44 +0530

    Use callback in `successive_approx` (#146)
    * Use callback in successive_approx
    
    * Use jax.jit in successive_approx

--- lectures/_static/lecture_specific/successive_approx.py
+++ lectures/_static/lecture_specific/successive_approx.py
@@ -1,13 +1,10 @@
-def successive_approx_jax(x_0,                   # Initial condition
-                          constants,
-                          sizes,
-                          arrays,                 
+def successive_approx_jax(T,                     # Operator (callable)
+                          x_0,                   # Initial condition                
                           tolerance=1e-6,        # Error tolerance
                           max_iter=10_000):      # Max iteration bound
-
     def body_fun(k_x_err):
         k, x, error = k_x_err
-        x_new = T(x, constants, sizes, arrays)
+        x_new = T(x)
         error = jnp.max(jnp.abs(x_new - x))
         return k + 1, x_new, error
 
@@ -18,4 +15,4 @@ def successive_approx_jax(x_0,                   # Initial condition
     k, x, error = jax.lax.while_loop(cond_fun, body_fun, (1, x_0, tolerance + 1))
     return x
 
-successive_approx_jax = jax.jit(successive_approx_jax, static_argnums=(2,))
+successive_approx_jax = jax.jit(successive_approx_jax, static_argnums=(0,))
